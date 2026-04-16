8b86e61ebe1f2361ff75dfeb08cf10a419afdf8c
Thiago Farias <thiagofarias13@hotmail.com>
2026-04-16 19:34:47 -0300

    Initial

---
+++ app/src/main/java/com/example/ovean_android_intro_abril_2026/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.ovean_android_intro_abril_2026
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val etNome = findViewById<EditText>(R.id.etNome)
+
+        btEnviar.setOnClickListener {
+            val nome = etNome.text.toString()
+
+            if (nome.isBlank()) {
+                etNome.error = "O nome não pode estar vazio"
+                etNome.requestFocus()
+            } else {
+                etNome.error = null
+                tvResultado.text = getString(R.string.boas_vindas, nome)
+            }
+        }
+    }
+}
